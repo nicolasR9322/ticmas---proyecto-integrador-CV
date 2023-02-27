edd495ca481a76c09beac9f4efce4d70db78f6f4
nicolasR9322 <nicolas.a.rodriguez93@gmail.com>
2023-02-27 08:06:58 -0300

    trabajo terminado

---
+++ index.js
@@ -0,0 +1,26 @@
+let menu = document.querySelector(".menu-barra");
+let closeBtn = document.querySelector(".close")
+let navbar = document.querySelector(".navbar")
+
+menu.addEventListener("click", abrirMenu)
+closeBtn.addEventListener("click", cerrarMenu)
+
+function abrirMenu() {
+    navbar.style.width = "100%";
+  }
+
+function cerrarMenu() {
+    navbar.style.width = "0";
+}
+
+let cerrarLinks = document.querySelectorAll(".link-close");
+
+for (let i = 0; i < cerrarLinks.length; i++) {
+  if(window.matchMedia("(min-width:768px)").matches){
+    cerrarLinks[i].addEventListener("click", abrirMenu)
+  } else {
+    cerrarLinks[i].addEventListener("click", cerrarMenu)
+  }
+  
+};
+
